3babe15bb6beefa5a8f227833b56077b562ee2d3
Anton Trafimovich <antont@compatibl.com>
2023-01-25 12:19:32 +0100

    feat: add member types routes.

--- src/routes/member-types/index.ts
+++ src/routes/member-types/index.ts
@@ -1,34 +1,62 @@
-import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-import { idParamSchema } from '../../utils/reusedSchemas';
-import { changeMemberTypeBodySchema } from './schema';
-import type { MemberTypeEntity } from '../../utils/DB/entities/DBMemberTypes';
+import { FastifyPluginAsyncJsonSchemaToTs } from "@fastify/type-provider-json-schema-to-ts";
+import { idParamSchema } from "../../utils/reusedSchemas";
+import { changeMemberTypeBodySchema } from "./schema";
+import type { MemberTypeEntity } from "../../utils/DB/entities/DBMemberTypes";
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise<
+  fastify.get("/", async function (request, reply): Promise<
     MemberTypeEntity[]
-  > {});
+  > {
+    return fastify.db.memberTypes.findMany();
+  });
 
   fastify.get(
-    '/:id',
+    "/:id",
     {
       schema: {
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      const { id } = request.params;
+
+      const result = await fastify.db.memberTypes.findOne({
+        key: "id",
+        equals: id,
+      });
+
+      if (result === null) {
+        return reply
+          .code(404)
+          .send({ error: `Mmeber type with id ${id} doesn't exist` });
+      }
+
+      return result;
+    }
   );
 
   fastify.patch(
-    '/:id',
+    "/:id",
     {
       schema: {
         body: changeMemberTypeBodySchema,
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      const memberTypeIdToUpdate = request.params.id;
+
+      try {
+        return await fastify.db.memberTypes.change(
+          memberTypeIdToUpdate,
+          request.body
+        );
+      } catch (err) {
+        return reply.code(404).send(err);
+      }
+    }
   );
 };
 
