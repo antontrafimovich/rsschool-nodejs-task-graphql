eb45f4859aba7310e4fa310f5144e537ac2fd8d0
Anton Trafimovich <antont@compatibl.com>
2023-01-25 12:14:26 +0100

    feat: add implementation to posts routes.

--- src/routes/posts/index.ts
+++ src/routes/posts/index.ts
@@ -1,52 +1,85 @@
-import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-import { idParamSchema } from '../../utils/reusedSchemas';
-import { createPostBodySchema, changePostBodySchema } from './schema';
-import type { PostEntity } from '../../utils/DB/entities/DBPosts';
+import { FastifyPluginAsyncJsonSchemaToTs } from "@fastify/type-provider-json-schema-to-ts";
+import { idParamSchema } from "../../utils/reusedSchemas";
+import { createPostBodySchema, changePostBodySchema } from "./schema";
+import type { PostEntity } from "../../utils/DB/entities/DBPosts";
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise<PostEntity[]> {});
+  fastify.get("/", async function (request, reply): Promise<PostEntity[]> {
+    return fastify.db.posts.findMany();
+  });
 
   fastify.get(
-    '/:id',
+    "/:id",
     {
       schema: {
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity> {
+      const { id } = request.params;
+
+      const result = await fastify.db.posts.findOne({
+        key: "id",
+        equals: id,
+      });
+
+      if (result === null) {
+        return reply
+          .code(404)
+          .send({ error: `Post with id ${id} doesn't exist` });
+      }
+
+      return result;
+    }
   );
 
   fastify.post(
-    '/',
+    "/",
     {
       schema: {
         body: createPostBodySchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity> {
+      return fastify.db.posts.create(request.body);
+    }
   );
 
   fastify.delete(
-    '/:id',
+    "/:id",
     {
       schema: {
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity> {
+      try {
+        return await fastify.db.posts.delete(request.params.id);
+      } catch (err) {
+        return reply.code(404).send(err);
+      }
+    }
   );
 
   fastify.patch(
-    '/:id',
+    "/:id",
     {
       schema: {
         body: changePostBodySchema,
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity> {
+      const profileIdToUpdate = request.params.id;
+
+      try {
+        return await fastify.db.posts.change(profileIdToUpdate, request.body);
+      } catch (err) {
+        return reply.code(404).send(err);
+      }
+    }
   );
 };
 
