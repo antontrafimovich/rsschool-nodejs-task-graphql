41a47c8842b971053e39a58d1b106bf91f7cbd49
Anton Trafimovich <antont@compatibl.com>
2023-01-25 12:10:17 +0100

    feat: add implementation to profile routes.

--- src/routes/profiles/index.ts
+++ src/routes/profiles/index.ts
@@ -1,54 +1,88 @@
-import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-import { idParamSchema } from '../../utils/reusedSchemas';
-import { createProfileBodySchema, changeProfileBodySchema } from './schema';
-import type { ProfileEntity } from '../../utils/DB/entities/DBProfiles';
+import { FastifyPluginAsyncJsonSchemaToTs } from "@fastify/type-provider-json-schema-to-ts";
+import { idParamSchema } from "../../utils/reusedSchemas";
+import { createProfileBodySchema, changeProfileBodySchema } from "./schema";
+import type { ProfileEntity } from "../../utils/DB/entities/DBProfiles";
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise<
-    ProfileEntity[]
-  > {});
+  fastify.get("/", async function (request, reply): Promise<ProfileEntity[]> {
+    return fastify.db.profiles.findMany();
+  });
 
   fastify.get(
-    '/:id',
+    "/:id",
     {
       schema: {
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      const { id } = request.params;
+
+      const result = await fastify.db.profiles.findOne({
+        key: "id",
+        equals: id,
+      });
+
+      if (result === null) {
+        return reply
+          .code(404)
+          .send({ error: `Profile with id ${id} doesn't exist` });
+      }
+
+      return result;
+    }
   );
 
   fastify.post(
-    '/',
+    "/",
     {
       schema: {
         body: createProfileBodySchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      return fastify.db.profiles.create(request.body);
+    }
   );
 
   fastify.delete(
-    '/:id',
+    "/:id",
     {
       schema: {
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      try {
+        return await fastify.db.profiles.delete(request.params.id);
+      } catch (err) {
+        return reply.code(404).send(err);
+      }
+    }
   );
 
   fastify.patch(
-    '/:id',
+    "/:id",
     {
       schema: {
         body: changeProfileBodySchema,
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      const profileIdToUpdate = request.params.id;
+
+      try {
+        return await fastify.db.profiles.change(
+          profileIdToUpdate,
+          request.body
+        );
+      } catch (err) {
+        return reply.code(404).send(err);
+      }
+    }
   );
 };
 
