6f0bc339f90ad96e6f16b35f53bf55b1644c9a03
Anton Trafimovich <antont@compatibl.com>
2023-01-26 13:17:46 +0100

    fix: add all the required validations for profiles endpoints.

--- src/routes/profiles/index.ts
+++ src/routes/profiles/index.ts
@@ -1,8 +1,9 @@
-import { FastifyPluginAsyncJsonSchemaToTs } from "@fastify/type-provider-json-schema-to-ts";
-import { idParamSchema } from "../../utils/reusedSchemas";
-import { createProfileBodySchema, changeProfileBodySchema } from "./schema";
-import type { ProfileEntity } from "../../utils/DB/entities/DBProfiles";
+import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 
+import { idParamSchema } from '../../utils/reusedSchemas';
+import { changeProfileBodySchema, createProfileBodySchema } from './schema';
+
+import type { ProfileEntity } from "../../utils/DB/entities/DBProfiles";
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
@@ -43,6 +44,45 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
       },
     },
     async function (request, reply): Promise<ProfileEntity> {
+      const { userId } = request.body;
+
+      const user = await fastify.db.users.findOne({
+        key: "id",
+        equals: userId,
+      });
+
+      if (user === null) {
+        return reply
+          .code(400)
+          .send(new Error(`User with id ${userId} doesn't exist.`));
+      }
+
+      const profile = await fastify.db.profiles.findOne({
+        key: "userId",
+        equals: userId,
+      });
+
+      if (profile !== null) {
+        return reply
+          .code(400)
+          .send(new Error(`User with id ${userId} already has profile.`));
+      }
+
+      const { memberTypeId } = request.body;
+
+      const memberType = await fastify.db.memberTypes.findOne({
+        key: "id",
+        equals: memberTypeId,
+      });
+
+      if (memberType === null) {
+        return reply
+          .code(400)
+          .send(
+            new Error(`Member type with id ${memberTypeId} doesn't exist.`)
+          );
+      }
+
       return fastify.db.profiles.create(request.body);
     }
   );
@@ -55,10 +95,12 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
       },
     },
     async function (request, reply): Promise<ProfileEntity> {
+      const { id } = request.params;
+
       try {
-        return await fastify.db.profiles.delete(request.params.id);
+        return await fastify.db.profiles.delete(id);
       } catch (err) {
-        return reply.code(404).send(err);
+        return reply.code(400).send(err);
       }
     }
   );
@@ -72,7 +114,7 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
       },
     },
     async function (request, reply): Promise<ProfileEntity> {
-      const profileIdToUpdate = request.params.id;
+      const { id: profileIdToUpdate } = request.params;
 
       try {
         return await fastify.db.profiles.change(
@@ -80,7 +122,7 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
           request.body
         );
       } catch (err) {
-        return reply.code(404).send(err);
+        return reply.code(400).send(err);
       }
     }
   );
