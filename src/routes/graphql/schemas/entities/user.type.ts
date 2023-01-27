d6afc98f559138c4f51be712129513f8f31306be
Anton Trafimovich <antont@compatibl.com>
2023-01-28 00:54:25 +0100

    feat: add ability to get users with their posts, profiles, memberTypes.

--- src/routes/graphql/schemas/entities/user.type.ts
+++ src/routes/graphql/schemas/entities/user.type.ts
@@ -4,12 +4,22 @@ import {
   GraphQLObjectType,
   GraphQLString,
 } from "graphql";
+import { PostEntity } from "../../../../utils/DB/entities/DBPosts";
 
 import { UserEntity } from "../../../../utils/DB/entities/DBUsers";
+import { ProfileEntity } from "../../../../utils/DB/entities/DBProfiles";
+import { MemberTypeEntity } from "../../../../utils/DB/entities/DBMemberTypes";
 import { ResolverContext } from "../model";
+import { profileType } from "./profile.type";
+
+type UserType = UserEntity & {
+  posts: PostEntity[];
+  profile: ProfileEntity;
+  memberType: MemberTypeEntity;
+};
 
 export const userType: GraphQLObjectType = new GraphQLObjectType<
-  UserEntity,
+  UserType,
   ResolverContext
 >({
   name: "User",
@@ -27,6 +37,33 @@ export const userType: GraphQLObjectType = new GraphQLObjectType<
     },
     email: {
       type: new GraphQLNonNull(GraphQLString),
+    },
+    posts: {
+      type: new GraphQLNonNull(profileType),
+      resolve: (user: UserEntity, _args, { db: { posts } }) => {
+        return posts.findMany({
+          key: "userId",
+          equals: user.id,
+        });
+      },
+    },
+    profile: {
+      type: new GraphQLNonNull(profileType),
+      resolve: (user: UserEntity, _args, { db: { profiles } }) => {
+        return profiles.findOne({
+          key: "userId",
+          equals: user.id,
+        });
+      },
+    },
+    memberType: {
+      type: new GraphQLNonNull(profileType),
+      resolve: (user: UserType, _args, { db: { memberTypes } }) => {
+        return memberTypes.findOne({
+          key: "id",
+          equals: user.profile.memberTypeId,
+        });
+      },
     },
     subscribedToUsers: {
       type: new GraphQLNonNull(userType),
